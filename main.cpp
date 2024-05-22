e6daf6f5f03964f4ffd34e2c5645e0157cdbfce3
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-05-21 22:25:11 -0700

    doing math

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
 #include <zoo/swar/SWAR.h>
 
 using u16 = uint16_t;
 static_assert(zoo::swar::SWAR<4, u16>{0b0000}.value() == 0);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    constexpr auto one = zoo::swar::SWAR<4, u16>{0b0001};
+    constexpr auto two = zoo::swar::SWAR<4, u16>{0b0010};
+    constexpr auto three = zoo::swar::SWAR<4, u16>{0b0011};
+
+    constexpr auto six = one + two + three;
+    static_assert(six.value() == 6);
+
     return 0;
 }
