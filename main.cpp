8be545a5b7e253a19a59a03e57ba0a53c9d9c58a
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-05-22 00:11:55 -0700

    ok add normal one too and a naieve approach

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "utils.h"
 #include "swar-strlen.h"
 
+constexpr static size_t naieve(const char *s) noexcept {
+    size_t i = 0;
+    while (s[i] != '\0') {
+        i++;
+    }
+    return i;
+}
+
 #define STRLEN_FUNCTIONS \
     X(_LIBC_STRLEN, strlen) \
-    X(_ZOO_STRLEN, c_strLength_natural) \
+    X(_ZOO_STRLEN_NORMAL, c_strLength) \
+    X(_ZOO_STRLEN_NATURAL, c_strLength_natural) \
     X(_ZOO_NATURAL_STRLEN, c_strLength_manualComparison) \
+    X(FUN, naieve) \
 
 #define X(Typename, FunctionToCall) \
     struct Invoke##Typename { int operator()(const char *p) { return FunctionToCall(p); } };
--- swar-strlen.h
+++ swar-strlen.h
@@ -70,3 +70,43 @@ static std::size_t c_strLength_manualComparison(const char *s) noexcept {
     }
   }
 }
+
+constexpr static std::size_t c_strLength(const char *s) noexcept {
+    using S = zoo::swar::SWAR<8, uint64_t>;
+    constexpr auto
+        MSBs = S{S::MostSignificantBit},
+        Ones = S{S::LeastSignificantBit};
+    constexpr auto BytesPerIteration = sizeof(S::type);
+    S initialBytes;
+
+    auto indexOfFirstTrue = [](auto bs) { return bs.lsbIndex(); };
+
+     // Misalignment must be taken into account because a SWAR read is
+    // speculative, it might read bytes outside of the actual string.
+    // It is safe to read within the page where the string occurs, and to
+    // guarantee that, simply make aligned reads because the size of the SWAR
+    // base size will always divide the memory page size
+    auto [alignedBase, misalignment] = blockAlignedLoad(s, &initialBytes.m_v);
+    auto bytes = adjustMisalignmentFor_strlen(initialBytes, misalignment);
+    for(;;) {
+        auto firstNullTurnsOnMSB = bytes - Ones;
+        // The first lane with a null will borrow and set its MSB on when
+        // subtracted one.
+        // The borrowing from the first null interferes with the subsequent
+        // lanes, that's why we focus on the first null.
+        // The lanes previous to the first null might keep their MSB on after
+        // subtracting one (if their value is greater than 0x80).
+        // This provides a way to detect the first null: It is the first lane
+        // in firstNullTurnsOnMSB that "flipped on" its MSB
+        auto cheapestInversionOfMSBs = ~bytes;
+        auto firstMSBsOnIsFirstNull =
+            firstNullTurnsOnMSB & cheapestInversionOfMSBs;
+        auto onlyMSBs = zoo::swar::convertToBooleanSWAR(firstMSBsOnIsFirstNull);
+        if(onlyMSBs) {
+            return alignedBase + indexOfFirstTrue(onlyMSBs) - s;
+        }
+        alignedBase += BytesPerIteration;
+        memcpy(&bytes, alignedBase, BytesPerIteration);
+    }
+}
+
