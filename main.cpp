69adf44e2afbe437667fc0bf2ca07c5d38ee7cfc
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-05-21 22:22:09 -0700

    works

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-
 #include <iostream>
+#include <zoo/swar/SWAR.h>
+
+using u16 = uint16_t;
+static_assert(zoo::swar::SWAR<4, u16>{0b0000}.value() == 0);
 
 int main() {
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
